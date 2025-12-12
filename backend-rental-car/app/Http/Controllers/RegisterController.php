d736c47cbddb6b8d164024d8290ccb84aec6082f
awiipp <sayidariipp@gmail.com>
2025-12-12 07:18:05 +0700

    bikin method show, update, destroy di register controller

--- backend-rental-car/app/Http/Controllers/RegisterController.php
+++ backend-rental-car/app/Http/Controllers/RegisterController.php
@@ -47,30 +47,91 @@ class RegisterController extends Controller
         return response()->json([
             'message' => 'create register success',
             'data' => $register,
-        ]);
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Register $register)
+    public function show(string $id)
     {
-        //
+        // cari register
+        $register = Register::find($id);
+
+        // kalo register ga ketemu
+        if (!$register) {
+            return response()->json([
+                'message' => 'register not found'
+            ], 404);
+        }
+
+        // kalau berhasil
+        return response()->json($register);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Register $register)
+    public function update(Request $request, string $id)
     {
-        //
+        // cari register
+        $register = Register::find($id);
+
+        // kalo ga ketemu
+        if (!$register) {
+            return response()->json([
+                'message' => 'register not found'
+            ], 404);
+        }
+
+        // validasi data untuk mengupdate register
+        $validator = Validator::make($request->all(), [
+            'no_ktp' => ['required'],
+            'name' => ['required'],
+            'date_of_birth' => ['required'],
+            'email' => ['required'],
+            'password' => ['required'],
+            'phone' => ['required'],
+            'description' => ['required'],
+        ]);
+
+        // kalau validasi gagal
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'invalid field',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        // kalau berhasil, update register
+        $register->update($validator->validated());
+
+        return response()->json([
+            'message' => 'register updated',
+            'data' => $register
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Register $register)
+    public function destroy(string $id)
     {
-        //
+        // cari register
+        $register = Register::find($id);
+
+        // kalo ga ketemu
+        if (!$register) {
+            return response()->json([
+                'message' => 'register not found'
+            ], 404);
+        }
+
+        // kalo ketemu, delete register
+        $register->delete();
+
+        return response()->json([
+            'message' => 'register deleted'
+        ]);
     }
 }
